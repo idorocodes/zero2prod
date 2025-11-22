7888c3f5a82204085c8bf0d36022bd5ab83b0729
idorocodes <idoroyen33@gmail.com>
2025-11-22 15:59:11 +0100

    Format code

---
+++ tests/tests.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn hello_print() {
+
+        println!("Hello, world!");
+        assert!(true);
+    }
+}
